34c9b25ecb10912783927ce5d57b2f756922d6db
HaoVN2411 <133398066+HaoVN2411@users.noreply.github.com>
2023-05-29 00:29:42 +0700

    First Commit

---
+++ src/java/Employee_Info/Employee_Info_DTO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Employee_Info;
+
+/**
+ *
+ * @author Hào Cute
+ */
+public class Employee_Info_DTO {
+    
+}
---
+++ src/java/User_Login_Controller/User_Login_DAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User_Login_Controller;
+
+/**
+ *
+ * @author Hào Cute
+ */
+public class User_Login_DAO {
+    
+}
